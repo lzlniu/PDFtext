e4cf4f89fef05c0ca320a726109c84722a02fd42
lzlniu <lzlniu@qq.com>
2020-12-14 09:51:01 +0100

    delete

--- PDFNetC64/Samples/SDFTest/JAVA/RunTest.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-setlocal
-set TEST_NAME=SDFTest
-javac.exe -cp .;../../../Lib/PDFNet.jar *.java
-java.exe -Djava.library.path=../../../Lib -classpath .;../../../Lib/PDFNet.jar %TEST_NAME%
-endlocal
